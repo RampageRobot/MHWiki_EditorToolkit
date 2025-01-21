01aff6b8c2701f8c6b864127789f4d51555080b6
Arrcival <arrcival@gmail.com>
2025-01-21 19:23:05 +0100

    feat: being able to move rows between neighboor tables

--- WebToolkit/wwwroot/js/TreeGenerator.js
+++ WebToolkit/wwwroot/js/TreeGenerator.js
@@ -74,9 +74,14 @@ function addRow(table, row, index = -1) {
         initRow();
         updateIcons();
     }
-    // Append at specified index
+    // Appends before at the specified index
+    // 0 means it'll append in the first row.
     else {
-        $(table).find("tbody > tr").eq(index).after(row);
+        if ($(table).find("tbody > tr").children().length == 0) {
+            $(table).append(row);
+        }
+        console.log(index)
+        $(table).find("tbody > tr").eq(index).before(row);
         initRow();
         updateIcons();
     }
@@ -110,7 +115,7 @@ function duplicateRow(row) {
     
     var duplicatedRow = $(srcTreeRow).clone(true);
     var table = $(row).parent().parent();
-    addRow(table, duplicatedRow, row.index())
+    addRow(table, duplicatedRow, row.index() - 1)
 
     duplicatedRow.find("[data-label=can-rollback]").prop("checked", row.find("[data-label=can-rollback]").is(":checked"))
 
@@ -190,4 +195,63 @@ function getDecos(levelSlot1, levelSlot2, levelSlot3) {
 
     var returnValue = JSON.stringify(decos);
     return returnValue;
+}
+
+function moveRowBefore(row) {
+    var rowIndex = getRowIndex(row);
+
+    // First row on the said table
+    if (rowIndex == 0) {
+        var treeContainer = row.closest("[class*='card-holder']");
+
+        var treeContainerIndex = treeContainer.index();
+        // There is a tree before the current tree, remove row and append at the end of that tree
+        if (treeContainerIndex > 0) {
+            var treesContainer = treeContainer.parent();
+            var container = $(treesContainer).children().eq(treeContainerIndex - 1);
+            var table = container.find('table');
+            row.parent().remove(row);
+            addRow(table, row);
+        }
+
+        var treesContainer = treeContainer.parent();
+
+        return;
+    }
+    moveButton.parents('.table-content-row').after(moveButton.parents('.table-content-row').prev())
+}
+
+function moveRowAfter(row) {
+    var rowIndex = getRowIndex(row);
+
+    // Last row on the said table
+    if (rowIndex == getRowsAmount(row.parent().parent()) - 1) {
+        var treeContainer = row.closest("[class*='card-holder']");
+
+        var treeContainerIndex = treeContainer.index();
+        var treesContainer = treeContainer.parent();
+        if (treeContainerIndex < treesContainer.children().length - 1) {
+            console.log("tree found after this one")
+            // There is a tree after the current tree, remove row and append at the start of that tree
+            var container = $(treesContainer).children().eq(treeContainerIndex + 1);
+            console.log(container);
+            var table = container.find('table');
+            console.log(table);
+            row.parent().remove(row);
+            addRow(table, row, 0);
+        }
+
+        var treesContainer = treeContainer.parent();
+
+        return;
+    }
+    moveButton.parents('.table-content-row').before(moveButton.parents('.table-content-row').next())
+}
+
+function getRowIndex(row) {
+    return row.parent().parent().find("tbody > tr").index(row);
+}
+
+function getRowsAmount(table) {
+    return table.find("tbody").children().length;
 }
