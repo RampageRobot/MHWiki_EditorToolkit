c08c7da53ed63967c98fd85e60f31df0ca50c121
Enis Avdovic <enis.avdovic@student.hs-rm.de>
2025-02-21 21:16:28 +0100

    Deleted print

--- WebToolkit/Program.cs
+++ WebToolkit/Program.cs
@@ -7,8 +7,6 @@ namespace WebToolkit
     {
         public static void Main(string[] args)
         {
-            string path = Directory.GetCurrentDirectory();
-            Console.WriteLine("The current directory is {0}", path);
 
             var builder = WebApplication.CreateBuilder(args);
 			// Add services to the container.
